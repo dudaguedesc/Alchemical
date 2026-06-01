537167c084973121bd5e1f63cf0bca0d1dddd29e
dudaguedesc <duda.guedes@edu.unirio.br>
2026-06-01 19:15:48 -0300

    Ativando registros no BD

--- src/managers/AuthManager.js
+++ src/managers/AuthManager.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 //(Project Settings > API)
 const SUPABASE_URL = 'https://wfokujwbgwnxtyxuyjih.supabase.co';
--- src/managers/RegisterManager.js
+++ src/managers/RegisterManager.js
@@ -1,38 +1,42 @@
-//import { register } from './AuthManager.js';
+import { AuthManager } from './AuthManager.js'; 
 
 export class RegisterManager
 {
- DoRegister(nomeJogador, email, password, confirmPassword)
- {
-  
-  if(this.verifyUser(nomeJogador) == false){
-    return {dados: null, erro: "Nome de jogador inválido"};
-  }
+    // usamos async porque a comunicaçao com a base de dados (supabase) nao e instantanea
+    async DoRegister(nomeJogador, email, password, confirmPassword)
+    {
+        // verificaçoes locais
+        if(this.verifyUser(nomeJogador) == false){
+            return {dados: null, erro: "Nome de jogador inválido"};
+        }
 
-  if(this.verifyEmail(email) == false){
-     return {dados: null, erro: "Email inválido"};
-  }
+        if(this.verifyEmail(email) == false){
+            return {dados: null, erro: "Email inválido"};
+        }
 
-  if(this.verifyPassword(password)== false){
-     return {dados: null, erro: "Senha inválida"};
-  }
-  if(this.verifyConfirmPassword(password, confirmPassword) == false){
-    return {dados: null, erro: "As senhas não coincidem"};
-  }
-   
-  //const resultado = register(nomeJogador, email, password);
-  const resultado = {dados: {nomeJogador, email, password}, erro: null}; // Simulação de resultado bem-sucedido
+        if(this.verifyPassword(password)== false){
+            return {dados: null, erro: "Senha inválida"};
+        }
+        
+        if(this.verifyConfirmPassword(password, confirmPassword) == false){
+            return {dados: null, erro: "As senhas não coincidem"};
+        }
+      
+        // o await faz com que o código espere a resposta da base de dados antes de continuar, garantindo que temos os dados ou o erro antes de prosseguir
+        const { data, error } = await AuthManager.register(email, password, nomeJogador);
 
-  //
-  return resultado;
-   
- }
-  verifyUser(nomeJogador){
-   if(nomeJogador.length >= 3){return true;} 
-   else{console.log("Nome de jogador inválido."); return false;}
-   } 
+        if (error) {
+            return {dados: null, erro: error.message}; 
+        }
+        return {dados: data, erro: null};
+    }
 
- verifyEmail(email)
+    verifyUser(nomeJogador){
+        if(nomeJogador.length >= 3){return true;} 
+        else{console.log("Nome de jogador inválido."); return false;}
+    } 
+
+    verifyEmail(email)
     { 
         if(email.includes("@")){return true;} 
         else{console.log("Email Inválido."); return false;}
@@ -40,12 +44,13 @@ export class RegisterManager
    
     verifyPassword(password)
     {
-        if(password.length >= 3){return true;}
+        if(password.length >= 6){return true;} 
         else{console.log("Senha Inválida."); return false;}
     }
+
     verifyConfirmPassword(password, confirmPassword)
     {
         if(password === confirmPassword){return true;}
-        else{console.log("As senhas não coincidem."); return false;}
+        else{console.log("As senhas não coincidem"); return false;}
     }
 }
